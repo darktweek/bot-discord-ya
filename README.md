464196b92cf7ebbfbe0b31f4c88f20575f2cd1b7
Tweek <pbst67@gmail.com>
2023-07-13 18:26:11 +0200

    ajouts de fonctions sur le bot

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A small bot for my Discord Server Yu'anang (a small community from StarCitizen player)
 
 Ce bot doit fonctionner avec un fichier .env qui contiendra "token=token-id" pour pouvoir fonctionner.
+
+commande pour démarrer le bot avec fichiers log
+nohup python3 bot-discord.py 1>bot-discord-log.out 2>bot-discord-log.err &
--- bot-discord.py
+++ bot-discord.py
@@ -9,19 +9,26 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
-
-class Message(ui.Modal, title='Message à envoyé sur Annonces'):
+class Message(ui.Modal, title='Message à envoyer sur Annonces'):
     texte = ui.TextInput(label='Message', style=discord.TextStyle.paragraph)
-    
     async def on_submit(self, interaction: discord.Interaction):
-        embed = discord.Embed(title = f"Annonce de {interaction.user}", description = f"{self.texte}")
-        embed.set_author(name = interaction.user, icon_url=interaction.user.avatar)
+        embed = discord.Embed(title = f"Annonce de {interaction.user}", description = f"{self.texte}", color = 0x5865F2)
+        embed.set_author(name = "Yu'ānang", icon_url='https://yuanang.space/wp-content/uploads/2023/07/logo_v2fondround.png')
         await interaction.response.send_message(f'{interaction.user}, ton message est envoyé', ephemeral=True)
-        channel = client.get_channel(1028728778890944613)
-        await channel.send(embed = embed)
+        global chanid
+        channel = client.get_channel(chanid)
+        await channel.send(f"@everyone", embed = embed)
 
-@tree.command(name = "message-vers-annonces", description = "Faire un message dans Annonces")
+@tree.command(name = "send-to-annonces", description = "Faire un message dans Annonces")
 async def react(interaction: discord.Interaction):
+    global chanid
+    chanid = 1128629326179483668   
+    await interaction.response.send_modal(Message())
+
+@tree.command(name = "send-to-annonces-int", description = "Faire un message dans Annonces Interne")
+async def react(interaction: discord.Interaction):
+    global chanid
+    chanid = 1007053041943461982   
     await interaction.response.send_modal(Message())
 
 @tree.command(name = "toolbox", description = "Donne l'url de la toolbox")
@@ -32,7 +39,9 @@ async def react(interaction: discord.Interaction):
 async def react(interaction: discord.Interaction):
     await interaction.response.send_message(f"{interaction.user} à demandé où introduire une demande. Il faut aller sur https://yuanang.space/envoyer-ma-demande/")
 
-
+@tree.command(name = "howitwork", description = "Des infos pour les curieux")
+async def react(interaction: discord.Interaction):
+    await interaction.response.send_message(f"{interaction.user} petit curieux! Tu trouvera tout le code à cette adresse https://github.com/darktweek/bot-discord-ya")
 
 @client.event
 async def on_ready():
