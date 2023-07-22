2574a394134a023a3a9f3511e2fea7d137aff85f
Tweek <pbst67@gmail.com>
2023-07-22 13:08:10 +0200

    ajout choix role everyone à ping

--- ya-dev-discord.py
+++ ya-dev-discord.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands, ui
 from discord.ext import commands
+from datetime import timedelta, datetime
 import os
 from dotenv import load_dotenv
 
@@ -13,16 +14,62 @@ intents.presences = True
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
-@tree.command(name = "ban", description = "Permet de BAN des Membres")
-async def react(ctx, membre: discord.Member, raison: str):
-    if  ctx.user.get_role(1131271268415586434):
-        channel = client.get_channel(1130508586926219275)
-        await ctx.guild.ban(membre)
-        await channel.send(f'💥 ← *{membre.name}* à été **ban** pour la raison: {raison}')
-        await ctx.response.send_message(f"Tu à **ban** {membre.name}", ephemeral=True)
-    else:
-        await ctx.response.send_message(f"Tu n'a pas la permission d'utiliser cette commande", ephemeral=True)
+HACK_OPTIONS = [
+   discord.SelectOption(label='Hacking', value='hack', description='did you experience someone hacking?'),
+   discord.SelectOption(label='Harassment', value='harass', description='did someone harass you?'),
+   discord.SelectOption(label='Discord ToS', value='tos', description='did you experience someone break the discord ToS in the server?'),
+   discord.SelectOption(label='Support', value='sup', description='do you need support?')
+]
 
+class HackView(discord.ui.View):
+    def __init__(self):
+      super().__init__(timeout=100)
+    @discord.ui.select(placeholder="Please select an option", options=HACK_OPTIONS, max_values=1)
+    async def reply_select(self, interaction: discord.Interaction, select: discord.ui.Select):
+      value = select.values[0]
+      if value == 'hack':
+         await interaction.response.send_message(f"You selected hack!")
+      else:
+         await interaction.response.send_message(f"You didn't select hack.")
+
+@tree.command(name='test', description='test')
+async def react(interaction: discord.Interaction):
+   await interaction.response.send_message(f"I'm here to help you", view=HackView())
+
+
+# Partie modal pour les commandes send-to
+class Message(ui.Modal, title='Message à envoyer'):
+    titre = ui.TextInput(label="Titre du message")
+    texte = ui.TextInput(label='Message', style=discord.TextStyle.paragraph)
+    async def on_submit(self, interaction: discord.Interaction):
+        global chanid
+        global reponse
+        channel = client.get_channel(chanid)
+        embed = discord.Embed(title = f"{self.titre}", description = f"{self.texte}", color = 0x5865F2)
+        embed.set_footer(text = f"Message de *{interaction.user}*")
+        await interaction.response.send_message(f'*{interaction.user}*, ton message est envoyé sur <#{chanid}>', ephemeral=True)
+        if reponse == 1:
+            await channel.send(f"@everyone", embed = embed)
+        else:
+            await channel.send(embed = embed)
+
+# Commande send-to avec restriction
+@tree.command(name = "envoyer-une-info", description = "Envoyer une info dans le tracker")
+@app_commands.describe(ping="Voulez-vous mentionner @everyone ?")
+@app_commands.rename(ping='ping_everyone')
+@app_commands.choices(ping=[
+        app_commands.Choice(name="Oui", value=1),
+        app_commands.Choice(name="Non", value=2)
+    ])
+async def react(interaction: discord.Interaction, ping : app_commands.Choice[int]):
+    if  interaction.user.get_role(1131271268415586434):
+        global chanid
+        global reponse
+        chanid = 1131561714668806227   
+        reponse = ping.value
+        await interaction.response.send_modal(Message())
+    else:
+        await interaction.response.send_message(f"Tu n'a pas la permission d'utiliser cette commande", ephemeral=True)
 
 @client.event
 async def on_ready():
--- ya-prod-discord.py
+++ ya-prod-discord.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands, ui
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -13,34 +14,54 @@ intents.presences = True
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
-# Partie modal pour les commandes send-to
+# Partie modal pour les commandes envoyer sur annonce
 class Message(ui.Modal, title='Message à envoyer'):
     titre = ui.TextInput(label="Titre du message")
     texte = ui.TextInput(label='Message', style=discord.TextStyle.paragraph)
     async def on_submit(self, interaction: discord.Interaction):
         global chanid
+        global reponse
         channel = client.get_channel(chanid)
         embed = discord.Embed(title = f"{self.titre}", description = f"{self.texte}", color = 0x5865F2)
         embed.set_footer(text = f"Message de *{interaction.user}*")
         await interaction.response.send_message(f'*{interaction.user}*, ton message est envoyé sur <#{chanid}>', ephemeral=True)
-        await channel.send(f"@everyone", embed = embed)
+        if reponse == 1:
+            await channel.send(f"@everyone", embed = embed)
+        else:
+            await channel.send(embed = embed)
 
-# Commande send-to avec restriction
-@tree.command(name = "send-to-annonces", description = "Faire un message dans Annonces")
-async def react(interaction: discord.Interaction):
+# Commande envoyer sur annonces avec restriction
+@tree.command(name = "envoyer-une-annonce", description = "Faire un message dans Annonces")
+@app_commands.describe(ping="Voulez-vous mentionner @everyone ?")
+@app_commands.rename(ping='ping_everyone')
+@app_commands.choices(ping=[
+        app_commands.Choice(name="Oui", value=1),
+        app_commands.Choice(name="Non", value=2)
+    ])
+async def react(interaction: discord.Interaction, ping : app_commands.Choice[int]):
     if  interaction.user.get_role(1007723429757194260):
         global chanid
-        chanid = 1028728778890944613   
+        chanid = 1028728778890944613
+        global reponse
+        reponse = ping.value
         await interaction.response.send_modal(Message())
     else:
         await interaction.response.send_message(f"Tu n'a pas la permission d'utiliser cette commande", ephemeral=True)
 
-# Commande send-to avec restriction
-@tree.command(name = "send-to-annonces-int", description = "Faire un message dans Annonces Interne")
-async def react(interaction: discord.Interaction):
+# Commande envoyer sur interne avec restriction
+@tree.command(name = "envoyer-une-annonce-interne", description = "Faire un message dans Annonces Interne")
+@app_commands.describe(ping="Voulez-vous mentionner @everyone ?")
+@app_commands.rename(ping='ping_everyone')
+@app_commands.choices(ping=[
+        app_commands.Choice(name="Oui", value=1),
+        app_commands.Choice(name="Non", value=2)
+    ])
+async def react(interaction: discord.Interaction, ping : app_commands.Choice[int]):
     if  interaction.user.get_role(1007723429757194260):
         global chanid
-        chanid = 1100114343758139483   
+        chanid = 1100114343758139483
+        global reponse
+        reponse = ping.value
         await interaction.response.send_modal(Message())
     else:
         await interaction.response.send_message(f"Tu n'a pas la permission d'utiliser cette commande", ephemeral=True)
