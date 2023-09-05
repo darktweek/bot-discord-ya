be398eccf90fe68e0084bf000161b32e35708d5c
Tweek <pbst67@gmail.com>
2023-09-05 14:26:29 +0200

    oublis d'un mot

--- var_prod/list.py
+++ var_prod/list.py
@@ -3,4 +3,4 @@ hello_list = ['*hello*', '*salut*', 'yo', 'yoo*', '*bonjour*', '*hugh*', '*hey*'
 bonapp_list = ['*bon app*', '*bonn app*']
 felicitations_list = ['gg', '*félicitation*', '*felicitation*']
 fete_list = ['*bonne année*', '*joyeux no*', '*happy birthday*', '*bon anniversaire*', '*bonne anne*',]
-bye_list = ['*bonne nuit*', '*a+*', '*bye*', '*nuitée*', '*tchao*', '*tcho*' ]
+bye_list = ['*bonne nuit*', '*a+*', '*bye*', '*nuitée*', '*tchao*', '*tcho*', '*good night*' ]
